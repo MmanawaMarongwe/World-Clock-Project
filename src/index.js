27f774446271e5baa38391afff819c75a1727605
Mmanawa Thato Marongwe <mashalammanawa@gmail.com>
2023-10-26 15:07:19 +0200

    deleted some js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,24 @@
 function updateTime(){
 // Los Angeles
-
 let losAngelesElement = document.querySelector("#los-angeles");
-if (losAngelesElement.length){
 losAngelesDateElement = losAngelesElement.querySelector(".date");
 losAngelesTimeElement = losAngelesElement.querySelector(".time");
 let losAngelesTime = moment().tz("America/Los_Angeles");
 
 losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
 losAngelesTimeElement.innerHTML = `${losAngelesTime.format('h:mm:ss [<small>]A[</small>]')}`;
-}
-// Lagos
 
+// Lagos
 let lagosElement = document.querySelector("#lagos");
-if (lagosElement.length){
 lagosDateElement = lagosElement.querySelector(".date");
 lagosTimeElement = lagosElement.querySelector(".time");
 let lagosTime = moment().tz("Africa/Lagos");
 
 lagosDateElement.innerHTML = lagosTime.format("MMMM Do YYYY");
 lagosTimeElement.innerHTML = `${lagosTime.format('h:mm:ss [<small>]A[</small>]')}`;
-}
-// Tokyo
 
+// Tokyo
 let tokyoElement = document.querySelector("#tokyo");
-if (tokyoElement.length){
 tokyoDateElement = tokyoElement.querySelector(".date");
 tokyoTimeElement = tokyoElement.querySelector(".time");
 let tokyoTime = moment().tz("Asia/Tokyo");
@@ -33,10 +27,6 @@ tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
 tokyoTimeElement.innerHTML = `${tokyoTime.format('h:mm:ss [<small>]A[</small>]')}`;
 }
 
-}
-
-updateTime();
-setInterval(updateTime, 1000)
 
 function updateCity(event){
 let citiesTimeZone = event.target.value;
@@ -53,6 +43,10 @@ cityElement.innerHTML = `
 </div>`
 
 }
+
+
+updateTime();
+setInterval(updateTime, 1000)
 
 let citiesSelect = document.querySelector("#cities");
 citiesSelect.addEventListener("change", updateCity);
