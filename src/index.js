03e94ab457bc33536eb548e57b94bae85a3da823
Mmanawa Thato Marongwe <mashalammanawa@gmail.com>
2023-10-27 09:05:00 +0200

    Revert "Merge branch 'master' of https://github.com/MmanawaMarongwe/World-Clock-Project"
    This reverts commit 3df21d1d14cbc85d56cc58fd677896a283581328, reversing
    changes made to e1937cbd1b721b1563143a8b23da03afc2d3c541.

--- src/index.js
+++ src/index.js
@@ -1,58 +1,77 @@
-function updateTime(){
-// Los Angeles
+function updateTime() {
+    // Los Angeles
+    let losAngelesElement = document.querySelector("#los-angeles");
+    if (losAngelesElement) {
+      let losAngelesDateElement = losAngelesElement.querySelector(".date");
+      let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  
+        let losAngelesTime = moment().tz("America/Los_Angeles");
+        losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+        losAngelesTimeElement.innerHTML = losAngelesTime.format("h:mm:ss [<small>]A[</small>]");
+      
+    }
+  
+    // Lagos
+    let lagosElement = document.querySelector("#lagos");
+    if (lagosElement) {
+      let lagosDateElement = lagosElement.querySelector(".date");
+      let lagosTimeElement = lagosElement.querySelector(".time");
 
-let losAngelesElement = document.querySelector("#los-angeles");
-if (losAngelesElement.length){
-losAngelesDateElement = losAngelesElement.querySelector(".date");
-losAngelesTimeElement = losAngelesElement.querySelector(".time");
-let losAngelesTime = moment().tz("America/Los_Angeles");
+        let lagosTime = moment().tz("Africa/Lagos");
+        lagosDateElement.innerHTML = lagosTime.format("MMMM Do YYYY");
+        lagosTimeElement.innerHTML = lagosTime.format("h:mm:ss [<small>]A[</small>]");
+    }
+  
+    // Tokyo
+    let tokyoElement = document.querySelector("#tokyo");
+    if (tokyoElement) {
+      let tokyoDateElement = tokyoElement.querySelector(".date");
+      let tokyoTimeElement = tokyoElement.querySelector(".time");
+  
+    
+        let tokyoTime = moment().tz("Asia/Tokyo");
+        tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
+        tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss [<small>]A[</small>]");
 
-losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
-losAngelesTimeElement.innerHTML = `${losAngelesTime.format('h:mm:ss [<small>]A[</small>]')}`;
-}
-// Lagos
+    }
+  }
+  
+  function updateCity(event) {
+    let citiesTimeZone = event.target.value;
+    let citiesTime = moment().tz(citiesTimeZone);
+    cityName = citiesTimeZone.split("/")[1]
+    let cityElement = document.querySelector("#city")
+    cityElement.innerHTML = `
+      <div class="city">
+        <div>
+          <h2>${cityName}</h2>
+          <div class="date">${citiesTime.format("MMMM Do YYYY")}</div>
+        </div>
+        <div class="time">${citiesTime.format('h:mm:ss [<small>]A[</small>]')}</div>
+      </div>`;
+  }
 
-let lagosElement = document.querySelector("#lagos");
-if (lagosElement.length){
-lagosDateElement = lagosElement.querySelector(".date");
-lagosTimeElement = lagosElement.querySelector(".time");
-let lagosTime = moment().tz("Africa/Lagos");
+  updateTime();
+  setInterval(updateTime, 1000);
+  
+  function updateCity(event) {
+        let citiesTimeZone = event.target.value;
+        let citiesTime = moment().tz(citiesTimeZone);
+        cityName = citiesTimeZone.split("/")[1]
+        let cityElement = document.querySelector("#city")
+        cityElement.innerHTML = ` 
+        <div class="city">
+        <div>
+          <h2>${cityName}</h2>
+          <div class="date">${citiesTime.format("MMMM Do YYYY")}</div>
+        </div>
+        <div class="time">${citiesTime.format('h:mm:ss [<small>]A[</small>]')}
+        </div>`
+        
 
-lagosDateElement.innerHTML = lagosTime.format("MMMM Do YYYY");
-lagosTimeElement.innerHTML = `${lagosTime.format('h:mm:ss [<small>]A[</small>]')}`;
-}
-// Tokyo
+  }
 
-let tokyoElement = document.querySelector("#tokyo");
-if (tokyoElement.length){
-tokyoDateElement = tokyoElement.querySelector(".date");
-tokyoTimeElement = tokyoElement.querySelector(".time");
-let tokyoTime = moment().tz("Asia/Tokyo");
-
-tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
-tokyoTimeElement.innerHTML = `${tokyoTime.format('h:mm:ss [<small>]A[</small>]')}`;
-}
-
-}
-
-updateTime();
-setInterval(updateTime, 1000)
-
-function updateCity(event){
-let citiesTimeZone = event.target.value;
-let citiesTime = moment().tz(citiesTimeZone);
-cityName = citiesTimeZone.split("/")[1]
-let cityElement = document.querySelector("#city")
-cityElement.innerHTML = ` 
-<div class="city">
-<div>
-  <h2>${cityName}</h2>
-  <div class="date">${citiesTime.format("MMMM Do YYYY")}</div>
-</div>
-<div class="time">${citiesTime.format('h:mm:ss [<small>]A[</small>]')}
-</div>`
-
-}
-
-let citiesSelect = document.querySelector("#cities");
-citiesSelect.addEventListener("change", updateCity);
+  
+  let citiesSelect = document.querySelector("#cities");
+  citiesSelect.addEventListener("change", updateCity);
+  
